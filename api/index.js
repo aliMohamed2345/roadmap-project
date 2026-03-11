cd071cee9e363c08d6a4a0615859d6e6ae9c9324
Ali Mohamed Mansor <137876199+aliMohamed2345@users.noreply.github.com>
2026-03-11 14:11:57 +0200

    changing the settings of cors

--- api/index.js
+++ api/index.js
@@ -25,7 +25,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(cors({ origin: [process.env.CLIENT_URL, "http://localhost:3000"], credentials: true }))
+app.use(cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 app.use(helmet());
 
 // Rate limiting: limit repeated requests to API
